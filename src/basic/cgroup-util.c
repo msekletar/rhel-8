d3ac002c080228bdb0874c676fede5ec41058b57
Lennart Poettering <lennart@poettering.net>
2018-10-24 17:31:51 +0200

    cgroup-util: before operating on a mounted cgroup controller check if it actually can be mounted
    We now have the "BPF" pseudo-controllers. These should never be assumed
    to be accessible as /sys/fs/cgroup/<controller> and not through
    "cgroup.subtree_control" either, hence always check explicitly before we
    go to the file system. We do this through our new CGROUP_MASK_V1 and
    CGROUP_MASK_V2 definitions.
    
    (cherry picked from commit ab275f23867311e136887755728e6b368324831d)
    
    Related: #1897579

--- src/basic/cgroup-util.c
+++ src/basic/cgroup-util.c
@@ -2150,6 +2150,9 @@ int cg_create_everywhere(CGroupMask supported, CGroupMask mask, const char *path
                 CGroupMask bit = CGROUP_CONTROLLER_TO_MASK(c);
                 const char *n;
 
+                if (!FLAGS_SET(CGROUP_MASK_V1, bit))
+                        continue;
+
                 n = cgroup_controller_to_string(c);
 
                 if (mask & bit)
@@ -2178,6 +2181,9 @@ int cg_attach_everywhere(CGroupMask supported, const char *path, pid_t pid, cg_m
         for (c = 0; c < _CGROUP_CONTROLLER_MAX; c++) {
                 CGroupMask bit = CGROUP_CONTROLLER_TO_MASK(c);
                 const char *p = NULL;
+
+                if (!FLAGS_SET(CGROUP_MASK_V1, bit))
+                        continue;
 
                 if (!(supported & bit))
                         continue;
@@ -2231,6 +2237,9 @@ int cg_migrate_everywhere(CGroupMask supported, const char *from, const char *to
                 CGroupMask bit = CGROUP_CONTROLLER_TO_MASK(c);
                 const char *p = NULL;
 
+                if (!FLAGS_SET(CGROUP_MASK_V1, bit))
+                        continue;
+
                 if (!(supported & bit))
                         continue;
 
@@ -2262,6 +2271,9 @@ int cg_trim_everywhere(CGroupMask supported, const char *path, bool delete_root)
 
         for (c = 0; c < _CGROUP_CONTROLLER_MAX; c++) {
                 CGroupMask bit = CGROUP_CONTROLLER_TO_MASK(c);
+
+                if (!FLAGS_SET(CGROUP_MASK_V1, bit))
+                        continue;
 
                 if (!(supported & bit))
                         continue;
@@ -2600,6 +2612,9 @@ int cg_enable_everywhere(CGroupMask supported, CGroupMask mask, const char *p) {
         for (c = 0; c < _CGROUP_CONTROLLER_MAX; c++) {
                 CGroupMask bit = CGROUP_CONTROLLER_TO_MASK(c);
                 const char *n;
+
+                if (!FLAGS_SET(CGROUP_MASK_V2, bit))
+                        continue;
 
                 if (!(supported & bit))
                         continue;
